9779eb06c3bdc732a22a225b355b79bc4e05347e
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-19 19:57:23 -0400

    backend

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,15 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect("mongodb://localhost/cookbooks_db", { useNewUrlParser: true });
+const { MONGODBURI } = process.env;
+const mongoose = require("mongoose");
+const config = { useUnifiedTopology: true, useNewUrlParser: true };
+const DB = mongoose.connection;
+
+mongoose.connect(MONGODBURI, config);
+
+DB.on("open", () => console.log("You are connected to Mongo"))
+  .on("close", () => console.log("You are disconnected to Mongo"))
+  .on("error", (err) => console.log(err));
 
 mongoose.Promise = Promise;
 
